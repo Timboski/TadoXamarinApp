b68f1ce98d6cb28d426218890ebc3d5724059504
Tim Boyle <timboboyle@yahoo.co.uk>
2019-12-15 22:54:12 +0000

    Format the time period in a more friendly way

---
+++ TadoXamarinApp/TadoXamarinApp/SecondsToFormattedTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TadoXamarinApp
+{
+    /// <summary>
+    /// Convert from: A time span in seconds (as an int)
+    /// Convert to: A fomatted time period Hours, Minutes and Seconds (as a string) 
+    /// </summary>
+    public class SecondsToFormattedTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+            => TimeSpan.FromSeconds((int)value).ToString();
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
